5b38c89c1229eeb18bd9bfdb1a6e2dadbfc5aef9
denis <denis.lipatov163@gmail.com>
2024-03-14 09:35:14 +0300

    reterned files

--- ArrayHelper/ArrayHelper.py
+++ ArrayHelper/ArrayHelper.py
@@ -1,24 +1,21 @@
 class ArrayHelper:
+    'A static class containing helper functions for array-related tasks.'
 
     @staticmethod
-    def __arg(value):
-        return ArrayHelper.clone(value)\
-            if isinstance(value, (dict, list)) else value
-
-    @staticmethod
-    def __have_clone(value):
-        return hasattr(value, 'clone')\
-            and callable(getattr(value, 'clone'))
+    def __get_clone_value(value):
+        'Returns a value, depending on the presence of a clone'
+        if hasattr(value, 'clone') and callable(getattr(value, 'clone')):
+            return value.clone()
+        return value
 
     @staticmethod
     def clone(arr: list):
+        'Returns a clone of the object'
         if not hasattr(arr, '__iter__'):
             return {}
         elif isinstance(arr, dict):
-            return {key: value.clone() if ArrayHelper.__have_clone(value)
-                    else ArrayHelper.__arg(value) for key, value in arr.items()}
+            return {key: ArrayHelper.__get_clone_value(value) for key, value in arr.items()}
         elif isinstance(arr, (list, tuple)):
-            return [value.clone() if ArrayHelper.__have_clone(value)
-                    else ArrayHelper.__arg(value) for value in arr]
+            return [ArrayHelper.__get_clone_value(value) for value in arr]
         elif isinstance(arr, str):
             return {str(i): symbol for i, symbol in enumerate(arr)}
