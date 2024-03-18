8905fdcb50e24d95fc2d3e6ffd6bfb88f04d8d2a
denis <denis.lipatov163@gmail.com>
2024-03-18 11:26:36 +0300

    deleted

--- done/ReactionParser.js
+++
@@ -1,9 +0,0 @@
-
-const Reaction = require('./Reaction')
-class ReactionParser {
-    static parse(reactionSmiles) {
-        let reaction = new Reaction(reactionSmiles);
-        return reaction;
-    }
-}
-module.exports = ReactionParser;
--- src/UtilityFunctions.py
+++
@@ -1,4 +0,0 @@
-def getChargeText(charge: float) -> str:
-    'Translate the integer indicating the charge to the appropriate text.'
-    charge_map = {1: '+', 2: '2+', -1: '-', -2: '2-'}
-    return charge_map.get(charge, '')
